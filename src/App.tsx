572be3f99bc3a02c8904bdea0bb613ab6093a084
Ariel Hurtado <arielhurtado388@gmail.com>
2024-12-12 18:31:48 -0500

    Movido a reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { useReducer } from "react";
 import MenuItem from "./components/MenuItem";
 import OrderContents from "./components/OrderContents";
 import OrderTotals from "./components/OrderTotals";
 import TipPercentageForm from "./components/TipPercentageForm";
 import { menuItems } from "./data/db";
-import useOrder from "./hooks/useOrder";
+import { initialState, orderReducer } from "./reducers/order-reducer";
 
 function App() {
-  const { order, tip, setTip, addItem, removeItem, placeOrder } = useOrder();
+  const [state, dispatch] = useReducer(orderReducer, initialState);
   return (
     <>
       <header className="bg-teal-400 py-5">
@@ -20,19 +21,23 @@ function App() {
           <h2 className="text-4xl font-black text-center">Menú</h2>
           <div className="space-y-3 mt-10">
             {menuItems.map((item) => (
-              <MenuItem key={item.id} item={item} addItem={addItem} />
+              <MenuItem key={item.id} item={item} dispatch={dispatch} />
             ))}
           </div>
         </div>
 
         <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-          {order.length ? (
+          {state.order.length ? (
             <>
-              <OrderContents order={order} removeItem={removeItem} />
+              <OrderContents order={state.order} dispatch={dispatch} />
 
-              <TipPercentageForm setTip={setTip} tip={tip} />
+              <TipPercentageForm dispatch={dispatch} tip={state.tip} />
 
-              <OrderTotals order={order} tip={tip} placeOrder={placeOrder} />
+              <OrderTotals
+                order={state.order}
+                tip={state.tip}
+                dispatch={dispatch}
+              />
             </>
           ) : (
             <p className="text-center">La orden está vacía</p>
--- src/components/TipPercentageForm.tsx
+++ src/components/TipPercentageForm.tsx
@@ -1,4 +1,5 @@
-import type { Dispatch, SetStateAction } from "react";
+import type { Dispatch } from "react";
+import { OrderActions } from "../reducers/order-reducer";
 
 const tipOptions = [
   {
@@ -19,12 +20,12 @@ const tipOptions = [
 ];
 
 type TipPercentageFormProps = {
-  setTip: Dispatch<SetStateAction<number>>;
+  dispatch: Dispatch<OrderActions>;
   tip: number;
 };
 
 export default function TipPercentageForm({
-  setTip,
+  dispatch,
   tip,
 }: TipPercentageFormProps) {
   return (
@@ -40,7 +41,12 @@ export default function TipPercentageForm({
               id={tipOption.id}
               name="tip"
               value={tipOption.value}
-              onChange={(e) => setTip(+e.target.value)}
+              onChange={(e) =>
+                dispatch({
+                  type: "add-tip",
+                  payload: { value: +e.target.value },
+                })
+              }
               checked={tipOption.value === tip}
             />
           </div>
